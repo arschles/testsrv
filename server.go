2c171984f5d16d352c2f8b35607a17ffe71bf377
Aaron Schlesinger <arschles@gmail.com>
2015-09-08 14:00:56 -0700

    better close sig

--- server.go
+++ server.go
@@ -3,53 +3,56 @@ package testsrv
 import (
 	"net/http"
 	"net/http/httptest"
+	"sync/atomic"
 	"time"
 
 	"code.google.com/p/go-uuid/uuid"
 )
 
-const (
-	UUIDHeaderName = "HTTP-Sub-UUID"
-)
-
-// ReceivedRequest represents a request that the subscriber received.
-type ReceivedRequest struct {
-	// The request the subscriber received
-	Request *http.Request
-	// The receipt time
-	Time time.Time
-	// Unique identifier of the request. will be returned as UUIDHeaderName in each
-	// response that the subscriber sends
-	UUID string
-}
-
-// Subscriber is a HTTP server that you can send real HTTP requests to
-// in your unit tests. In addition to the functionality of net/http/httptest,
-// it allows callers to easily retrieve the requests that came into the server
+// Server is a HTTP server that you can send real HTTP requests to in your unit tests.
+// Each request is handled by a http.Handler that you specify, and the server records each
+// request after your handler finishes execution. You can later retreive details about each
+// received request
 type Server struct {
-	ch  chan *ReceivedRequest
-	srv *httptest.Server
+	closed   int64
+	closeSig chan struct{} // closed when Close is called
+	ch       chan *ReceivedRequest
+	srv      *httptest.Server
 }
 
+// StartServer starts the server listening in the background. All requests sent to the server
+// at s.URLStr() are sent to handler, flushed and then recorded for later retrieval using the AcceptN func
 func StartServer(handler http.Handler) *Server {
 	ch := make(chan *ReceivedRequest)
-	wrappedHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	closeSig := make(chan struct{})
+	wrapper := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		u := uuid.New()
-		go func() {
-			// TODO: stop sending on s.Close()
-			recvReq := ReceivedRequest{Request: r, Time: time.Now(), UUID: u}
-			ch <- &recvReq
-		}()
-		w.Header().Add("HTTP-Sub-UUID", u)
+		w.Header().Add(UUIDHeaderName, u)
 		handler.ServeHTTP(w, r)
+		req := &ReceivedRequest{Request: r, Time: time.Now(), UUID: u}
+		select {
+		case <-closeSig:
+		case ch <- req:
+		}
 	})
-	s := httptest.NewServer(wrappedHandler)
-	return &Server{ch: ch, srv: s}
+
+	s := httptest.NewServer(wrapper)
+	return &Server{
+		closed:   0,
+		closeSig: closeSig,
+		ch:       ch,
+		srv:      s,
+	}
 }
 
 // Close releases all resources on this subscriber
 func (s *Server) Close() {
-	s.srv.Close()
+	if atomic.CompareAndSwapInt64(&s.closed, 0, 1) {
+		s.srv.Close()
+		s.srv.CloseClientConnections()
+		close(s.closeSig)
+		return
+	}
 }
 
 // URLStr returns the string representation of the URL to call to hit this server
@@ -67,9 +70,10 @@ func (s *Server) AcceptN(n int, maxWait time.Duration) []*ReceivedRequest {
 		select {
 		case recvReq := <-s.ch:
 			ret = append(ret, recvReq)
+		case <-s.closeSig:
+			return ret
 		case <-tmr:
 			return ret
 		}
 	}
-	return ret
 }
