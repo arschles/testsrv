0d9d1f713f5cb5bb2eeb332e92aed46b13bd3b49
Aaron Schlesinger <aaschles@microsoft.com>
2018-09-12 13:08:27 -0700

    fail a test to see what happens!

--- server_test.go
+++ server_test.go
@@ -29,6 +29,7 @@ func TestNoMsgs(t *testing.T) {
 	case <-time.After(recvWaitTime):
 		t.Errorf("AcceptN didn't return after [%+v]", recvWaitTime)
 	}
+	t.Fatalf("let's see what happens when we fail a test in Azure pipelines!")
 }
 
 func TestMsgs(t *testing.T) {
